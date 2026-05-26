01cd3a7d76ef005cf2b201348449f572ab121a15
Josiah Stoltzfus <Josiah4191@gmail.com>
2026-05-26 18:06:17 -0500

    Change env password

--- src/main/java/com/josiah/squirrels/squirrel/repository/SquirrelRepository.java
+++ src/main/java/com/josiah/squirrels/squirrel/repository/SquirrelRepository.java
@@ -4,6 +4,4 @@ package com.josiah.squirrels.squirrel.repository;
 import com.josiah.squirrels.squirrel.entity.Squirrel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SquirrelRepository extends JpaRepository<Squirrel, Long> {}
